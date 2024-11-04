9c93e33086711e0c533ac83ca212cd13c94e3592
warley <warleytho369@gmail.com>
2024-11-04 10:31:08 -0300

    .gitignore/atribute - bundling & minification

--- src/AppSemTemplate/obj/Debug/net8.0/AppSemTemplate.AssemblyInfo.cs
+++ src/AppSemTemplate/obj/Debug/net8.0/AppSemTemplate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppSemTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9ae5dffdb90853f2520c220b6393450239f5571")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppSemTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppSemTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
